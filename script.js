8c33fdeb1830210cc4e2a65d8145abf1fb453899
harry-2426 <johnharrynicer@gmail.com>
2025-03-05 23:23:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let pTags = document.getElementsByTagName("p");
+console.log(pTags);
+
+pTags[0].style.color = "black"
+pTags[1].style.color = "black"
+pTags[2].style.color = "black"
+pTags[3].style.color = "black"
+pTags[4].style.color = "black"
+pTags[5].style.color = "black"
+pTags[6].style.color = "black"
+pTags[7].style.color = "black"
+
+document.querySelector("img").style.backgroundColor = "white";
+
+
